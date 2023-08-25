621315883d941a1ea4967029b701a97139540db5
Parv Rehan <z5421600@ad.unsw.edu.au>
2023-08-25 15:20:15 +1000

    Updated TwoSum

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -7,13 +7,16 @@ public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         List<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toList());
 
-        for (Integer num : numbers) {
-            Integer pair = target - num;
+        List<Integer> sorted = new ArrayList<>(numbers);
+        sorted.sort(null);
 
-            List<Integer> dup = new ArrayList<>(numbers);
+        for (Integer num : sorted) {
+            int pair = target - num;
+
+            List<Integer> dup = new ArrayList<>(sorted);
             dup.remove(num);
 
-            if (dup.contains(pair)) {
+            if (binarySearch(dup, pair)) {
                 int[] indices = {numbers.indexOf(num), numbers.lastIndexOf(pair)};
                 return indices;
             }
@@ -22,11 +25,32 @@ public class TwoSum {
         return new int[0];
     }
 
-    public static void main(String[] args) {
-        int[] nums = {3, 3};
+    // Returns whether toFind is an element of values in log(n) time.
+    // @Pre-conditions: values is sorted.
+    public <T extends Comparable<T>> boolean binarySearch(List<T> values, T toFind) {
+        int midIndex = values.size() / 2;
+        int comparison = values.get(midIndex).compareTo(toFind);
 
+        if (values.size() == 0) return false;
+        if (values.size() == 1 && comparison != 0) return false;
+        
+        // Bottom-Half
+        if (comparison > 0) {
+            return binarySearch(values.subList(0, midIndex), toFind);
+        // Top-Half
+        } else if (comparison < 0) { 
+            return binarySearch(values.subList(midIndex, values.size()), toFind);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
         TwoSum s = new TwoSum();
-        s.twoSum(nums, 6);
+        s.twoSum(new int[]{3, 2, 3}, 6);
+        s.twoSum(new int[]{2, 7, 11, 15}, 9);
+        s.twoSum(new int[]{3, 3}, 6);
+        s.twoSum(new int[]{3, 2, 4}, 6);
 
     }
 }
