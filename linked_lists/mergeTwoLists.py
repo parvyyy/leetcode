e9216f4913b48e8f7b155d567c1818c7f0159cdd
Parv Rehan <z5421600@ad.unsw.edu.au>
2024-01-21 22:03:21 +1100

    Simplified Merge Two Lists logic

--- linked_lists/mergeTwoLists.py
+++ linked_lists/mergeTwoLists.py
@@ -1,5 +1,5 @@
 class ListNode(object):
-     def __init__(self, val=0, next=None):
+     def __init__(self, val = 0, next = None):
          self.val = val
          self.next = next
     
@@ -12,41 +12,32 @@ def mergeTwoLists(list1, list2):
      if head2 is None:
           return list1
      
-     node = None
-     merged_head = None
-
+     # 'Node' will be the travelling ptr. 'Dummy will be a pointing to the
+     # head of the merged LL.
+     dummy = node = ListNode()
+     
      while head1 is not None and head2 is not None:
           if (head1.val <= head2.val):
-               if not node:
-                    node = ListNode(head1.val, None)
-                    merged_head = node
-               else:
-                    node.next = ListNode(head1.val, None)
-                    node = node.next
-
+               # Create next node
+               node.next = ListNode(head1.val, None)
                head1 = head1.next
           else:
-               if not node:
-                    node = ListNode(head2.val, None)
-                    merged_head = node
-               else:
-                    node.next = ListNode(head2.val, None)
-                    node = node.next
-
+               # Create next node
+               node.next = ListNode(head2.val, None)
                head2 = head2.next
-     
-     # Add the leftover elements into merged
-     while head1 is not None:
-          node.next = ListNode(head1.val, None)
-          head1 = head1.next
-          node = node.next
-     
-     while head2 is not None:
-          node.next = ListNode(head2.val, None)
-          head2 = head2.next
-          node = node.next
           
-     return merged_head
+          # Increments the merged LL pointer to the latest element.
+          node = node.next
+     
+     # Add the leftover elements into merged. This can be done by simply linking
+     # the end of the current merged LL to the traversal ptr of the longer LL.
+     if head1:
+          node.next = head1
+
+     if head2:
+          node.next = head2
+          
+     return dummy.next
 
 def main():
      merged_list = mergeTwoLists(ListNode(1, ListNode(2, ListNode(4, None))), ListNode(1, ListNode(3, ListNode(4, None))))
